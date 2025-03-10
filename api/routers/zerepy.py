e1b6582d7b884b1e654dc5c8083a60b9481c160e
sekomer <a.serkanaksoz@gmail.com>
2025-03-10 15:06:39 +0300

    feat: Add ZerepyRequestV2 endpoint with chat history support

--- api/routers/zerepy.py
+++ api/routers/zerepy.py
@@ -44,9 +44,6 @@ def zerepy_request(db: db_dependency, user: user_dependency, request: ZerepyRequ
     account = Account.from_key(pk)
     address = account.address
 
-    system_prompt = get_system_prompt(agent)
-    print("dududududuudud", system_prompt)
-
     response = openai_agent.structured_call(
         system_prompt=get_system_prompt(agent),
         messages=[
@@ -61,6 +58,79 @@ def zerepy_request(db: db_dependency, user: user_dependency, request: ZerepyRequ
                 """,
             }
         ],
+    )
+    print(response)
+
+    res = process_response(response, private_key=pk)
+    print("res status", res["status"])
+    print("res result", res["result"])
+
+    return ZerepyResponse(
+        status=res["status"], action=res["action"], result=res["result"]
+    )
+
+
+class ChatMessage(BaseModel):
+    id: int
+    conversation_id: int
+    role: str
+    content: str
+    created_at: str
+
+
+class ZerepyRequestV2(BaseModel):
+    chat_history: List[ChatMessage]
+    agent_id: int
+
+
+@router.post("/v2", status_code=status.HTTP_201_CREATED, response_model=ZerepyResponse)
+def zerepy_request_v2(
+    db: db_dependency, user: user_dependency, request: ZerepyRequestV2
+):
+    # 1 user => pk
+    # 2 prompt
+
+    print("user", user)
+
+    agent = db.query(AIAgent).filter(AIAgent.id == request.agent_id).first()
+    print("agent", agent)
+
+    pk = agent.evm_private_key
+
+    account = Account.from_key(pk)
+    address = account.address
+
+    system_prompt = get_system_prompt(agent)
+
+    messages = [
+        {
+            "role": message.role,
+            "content": message.content,
+        }
+        for message in request.chat_history
+    ]
+
+    # find last message with role "user" and add a prefix to the content
+    last_user_message = next(
+        (message for message in messages if message["role"] == "user"), None
+    )
+    if last_user_message:
+        last_user_message["content"] = (
+            f"""
+            helper informations:
+            user address is: {address}
+            --------------------------------------------------------
+            below is the user prompt:
+            {last_user_message['content']}
+            """
+        )
+
+    print("last user message", last_user_message)
+    print("messages", messages)
+
+    response = openai_agent.structured_call(
+        system_prompt=get_system_prompt(agent),
+        messages=messages,
     )
     print(response)
 
